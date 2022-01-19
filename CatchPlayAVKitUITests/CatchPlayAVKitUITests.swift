914f348196a00462b0de7a281f2ba762fbaed5c3
Astrid <tingan.l1216@gmail.com>
2022-01-19 14:21:59 +0800

    Make UITest more readable.

--- CatchPlayAVKitUITests/CatchPlayAVKitUITests.swift
+++ CatchPlayAVKitUITests/CatchPlayAVKitUITests.swift
@@ -18,47 +18,55 @@ class CatchPlayAVKitUITests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        app = nil
+        app.terminate()
     }
 
     func test_tapPlayOnLaunchPage_playerPagePlayButtonShouldExist() throws {
-        LaunchPage(app).presentPlayerPage()
+        LaunchPage(app)
+            .presentPlayerPage()
         XCTAssertTrue(PlayerPage(app).playerPlayButton.exists)
     }
     
     func test_tapDismissButtonOnPlayerPage_launchPagePlayButtonShouldExist() throws {
-        LaunchPage(app).presentPlayerPage()
-        PlayerPage(app).dismissButton.tap()
+        LaunchPage(app)
+            .presentPlayerPage()
+            .dismissPlayerPage()
         XCTAssertTrue(LaunchPage(app).playButton.exists)
     }
     
     func test_lockScreen_playerPagePlayButtonShouldNotExist() throws {
-        LaunchPage(app).presentPlayerPage()
-        PlayerPage(app).lockButton.tap()
+        LaunchPage(app)
+            .presentPlayerPage()
+            .lockScreen()
         XCTAssertFalse(PlayerPage(app).playerPlayButton.exists)
     }
     
     func test_unlockScreen_playerPagePlayButtonShouldExist() {
-        LaunchPage(app).presentPlayerPage()
-        PlayerPage(app).lockButton.tap()
-        PlayerPage(app).unlockButton.tap()
+        LaunchPage(app)
+            .presentPlayerPage()
+            .lockScreen()
+            .unlockScreen()
         XCTAssertTrue(PlayerPage(app).playerPlayButton.exists)
     }
     
     func test_playerControlAutoHide_playerPagePlayButtonShouldNotExistAfter5Secs() {
-        LaunchPage(app).presentPlayerPage()
-        PlayerPage(app).playerPlayButton.tap()
+        LaunchPage(app)
+            .presentPlayerPage()
+            .playVideo()
         sleep(5)
         XCTAssertFalse(PlayerPage(app).playerPlayButton.exists)
     }
     
     func test_tapAudioSubtitleButton_subtitleAudioPageTableViewShouldExist() {
-        LaunchPage(app).presentPlayerPage()
-        PlayerPage(app).subtitleAudioButton.tap()
+        LaunchPage(app)
+            .presentPlayerPage()
+            .presentSubtitleAudioPage()
         XCTAssertTrue(SubtitleAudioPage(app).audioTableView.exists)
     }
 
 }
+
+// MARK: - Page Object Pattern
 
 class Page {
     var app: XCUIApplication
@@ -68,18 +76,55 @@ class Page {
 }
 
 class LaunchPage: Page {
+    
     lazy var playButton = app.buttons["play"].firstMatch
-    func presentPlayerPage() {
+    
+    @discardableResult
+    func presentPlayerPage() -> PlayerPage {
         playButton.tap()
+        return PlayerPage(app)
     }
+    
 }
 
 class PlayerPage: Page {
+    
     lazy var playerPlayButton = app.buttons["playImageButton"].firstMatch
     lazy var lockButton = app.buttons["lock"].firstMatch
     lazy var unlockButton = app.buttons["unlockButton"].firstMatch
     lazy var dismissButton = app.buttons["dismissButton"].firstMatch
     lazy var subtitleAudioButton = app.buttons["Subtitle/Audio"].firstMatch
+    
+    @discardableResult
+    func lockScreen() -> Self {
+        lockButton.tap()
+        return self
+    }
+    
+    @discardableResult
+    func unlockScreen() -> Self {
+        unlockButton.tap()
+        return self
+    }
+    
+    @discardableResult
+    func dismissPlayerPage() -> Self {
+        dismissButton.tap()
+        return self
+    }
+    
+    @discardableResult
+    func playVideo() -> Self {
+        playerPlayButton.tap()
+        return self
+    }
+    
+    @discardableResult
+    func presentSubtitleAudioPage() -> SubtitleAudioPage {
+        subtitleAudioButton.tap()
+        return SubtitleAudioPage(app)
+    }
+    
 }
 
 class SubtitleAudioPage: Page {
